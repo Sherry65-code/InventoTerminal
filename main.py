efe20b80802294f18ea7a88eb50c0ff41b2bc081
Sherry65-Code <sparambir65@gmail.com>
2022-05-01 19:12:51 +0530

    main

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
-from ast import expr_context
 from os import system, getcwd
+from colorama import Fore
 def clear():
     system("clear")
 
 clear()
-print("Invento Terminal")
+print(Fore.YELLOW+"Invento Terminal")
 while True:
-    cmd = input(f"InTe({getcwd()})>>>")
+ try:
+    print(Fore.GREEN+"InTe",end="")
+    print(f"@{getcwd()}~",end="")
+    cmd = input(Fore.WHITE)
     commandArray = cmd.split(' ')
     if commandArray[0] == "print":
         g = 0
@@ -45,9 +48,13 @@ while True:
     elif commandArray[0] == "delete":
         system(f"sudo rm -r {commandArray[1]}")
     elif commandArray[0] == "file":
-        system(f"sudo nano {commandArray[1]}")
+        system(f"touch ./{commandArray[1]}")
     elif commandArray[0] == "ld":
         system("ls")
+    elif commandArray[0] == "la":
+        system('ls -a')
+    elif commandArray[0] == "lal":
+        system('ls -al')
     elif commandArray[0] == "cd":
         system(f"cd {commandArray[1]}")
     elif commandArray[0] == "folder":
@@ -56,7 +63,29 @@ while True:
         system(f"less {commandArray[1]}")
     elif commandArray[0] == "log":
         clear()
-        system("less ~/.log_Invento_Terminal.log")
+        system("cat ~/.log_Invento_Terminal.log")
+    elif commandArray[0] == 'se':
+        system('cat ~/.error.txt')
+    elif commandArray[0] == 'ls':
+        print("That is a Bash command and you are using InVe Shell. So you should use ld to list directory, la to list directory with hidden files and use lal to list directories with all information avaliable to the Shell.")
+    elif commandArray[0] == 'edit':
+        system(f'nano {commandArray[1]}')
+    elif commandArray[0] == 'get':
+        system(f'echo "Downloading && Installing..." && sudo apt install --assume-yes {commandArray[1]} > .error.txt')
+    elif commandArray[0] == 'geth':
+        system(f'echo "Downloading && Installing..." && sudo apt install --assume-yes {commandArray[1]}')
+    elif commandArray[0] == 'getha':
+        system(f'echo "Downloading && Installing..." && sudo apt install {commandArray[1]}')
+    elif commandArray[0] == 'qmake':
+        system('make')
+    elif commandArray[0] == 'qmakei':
+        system('sudo make install')
+    elif commandArray[0] == 'read':
+        system(f'cat {commandArray[1]}')
+    elif commandArray[0] == 'sys':
+        system(f'systemctl {commandArray[1]}')
     else:
         print(f"Error. '{commandArray[0]}' is an unknown command")
-    
+ except Exception as e:
+     system(f'echo "{e}" > ~/.error.txt')
+     print("An Unknown exception occured. try 'se' to check the error.")
